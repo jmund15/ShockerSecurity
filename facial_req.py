0678bbf536d9fdda003a219906a5e7c4b6fc9928
jmund15 <jmundy2001@gmail.com>
2024-04-02 17:16:03 -0500

    updated quitting

--- facial_req.py
+++ facial_req.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 # import the necessary packages
-from picamera2 import Picamera2, MappedArray
+from picamera2 import Picamera2, MappedArray, Preview
 from imutils.video import VideoStream
 from imutils.video import FPS
 import face_recognition
@@ -9,9 +9,8 @@ import imutils
 import pickle
 import time
 import cv2
+import keyboard
 
-#Initialize 'currentname' to trigger only when a new person is identified.
-currentname = "unknown"
 #Determine faces from encodings.pickle file model created from train_model.py
 encodingsP = "encodings.pickle"
 
@@ -26,15 +25,13 @@ data = pickle.loads(open(encodingsP, "rb").read())
 
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480),}))#, lores={"size": (320, 240), "format": "YUV420"}))
+picam2.start_preview(Preview.QTGL, width=1280, height=960)
 
 (w0, h0) = picam2.stream_configuration("main")["size"] 
 #(w1, h1) = picam2.stream_configuration("lores")["size"]
 
 #cv2.namedWindow("Facial Detection", cv2.WINDOW_NORMAL)
 #cv2.resizeWindow("Facial Detection", 1200, 800)
-
-# start the FPS counter
-fps = FPS().start()
 
 def draw_faces(request):
 	with MappedArray(request, "main") as m:
@@ -66,10 +63,11 @@ def draw_faces(request):
 				name = max(counts, key=counts.get)
 
 				#If someone in your dataset is identified, print their name on the screen
-				if currentname != name:
-					currentname = name
-					print(currentname)
-
+				#if currentname != name:
+				#	currentname = name
+				#	print(currentname)
+				#else:
+				#	currentname = "Unknown"
 			# update the list of names
 			names.append(name)
 
@@ -80,12 +78,16 @@ def draw_faces(request):
 			
 			# draw the predicted face name on the image - color is in BGR
 			cv2.rectangle(m.array, (left, top), (right, bottom),
-				(0, 255, 225), 2)
+				(0, 255, 225), 0)
 			y = top - 15 if top - 15 > 15 else top + 15
 			cv2.putText(m.array, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-				.8, (0, 255, 255), 2)
-	
-endcodings = []
+				.8, (0, 255, 255), 0)
+
+boxes = []
+encodings = []
+names = []
+#Initialize 'currentname' to trigger only when a new person is identified.
+currentname = "Unknown"
 
 picam2.post_callback = draw_faces
 picam2.start(show_preview = True)
@@ -99,23 +101,28 @@ while True:
 	boxes = face_recognition.face_locations(rgb)
 	# compute the facial embeddings for each face bounding box
 	encodings = face_recognition.face_encodings(rgb, boxes)
+	
 	names = []
-
+	
+	if keyboard.is_pressed('q'):
+		break
 	# display the image to our screen
 	#cv2.imshow("Facial Detection", frame)
-	key = cv2.waitKey(1) & 0xFF
+	#key = cv2.waitKey(1)# & 0xFF
+	#if key%256 == 27:
+	#	break
 	# quit when 'q' key is pressed
-	if key == ord("q"):
-		break
-
+	#if key == ord("q"):
+    #	break
+    
+	#print(key)
 	# update the FPS counter
-	fps.update()
 
-# stop the timer and display FPS information
-fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
+picam2.stop_preview()
+picam2.stop()
 #vs.stop()
