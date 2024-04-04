03cdaabafe823b943c1e87169399fc46ea3f3290
jmund <jmund@niar.wichita.edu>
2024-04-03 19:19:52 -0500

    fix hopefully

--- picam.py
+++ picam.py
@@ -59,22 +59,20 @@ knownNames = []
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
 	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
+	print("[INFO] processing image {}/{}, {}".format(i + 1,
+		len(imagePaths), imagePath))
 	#name = imagePath.split(os.path.sep)[-2]
 
 	# load the input image and convert it from RGB (OpenCV ordering)
 	# to dlib ordering (RGB)
 	image = cv2.imread(imagePath)
-	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 	# detect the (x, y)-coordinates of the bounding boxes
 	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model="hog")
-
+	face_locations = face_recognition.face_locations(rgb)
 	# compute the facial embedding for the face
-	encodings = face_recognition.face_encodings(rgb, boxes)
+	encodings = face_recognition.face_encodings(rgb, face_locations)
 
 	# loop over the encodings
 	for encoding in encodings:
