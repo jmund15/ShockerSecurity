ec923f326a49df8c78166b4dadcec23608b5d84e
jmund <jmund@niar.wichita.edu>
2024-04-03 20:25:44 -0500

    fix AGAIN

--- facial_req.py
+++ facial_req.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 # import the necessary packages
-from picamera2 import Picamera2
+from picamera2 import Picamera2, MappedArray
 from imutils.video import VideoStream
 from imutils.video import FPS
 import face_recognition
@@ -28,62 +28,64 @@ picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480),}))#, lores={"size": (320, 240), "format": "YUV420"}))
 
 (w0, h0) = picam2.stream_configuration("main")["size"] 
-(w1, h1) = picam2.stream_configuration("lores")["size"]
+#(w1, h1) = picam2.stream_configuration("lores")["size"]
 
-cv2.namedWindow("Facial Detection", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Facial Detection", 1200, 800)
+#cv2.namedWindow("Facial Detection", cv2.WINDOW_NORMAL)
+#cv2.resizeWindow("Facial Detection", 1200, 800)
 
 # start the FPS counter
 fps = FPS().start()
 
-def draw_faces(encodings):
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
-		name = "Unknown" #if face is not recognized, then print Unknown
+def draw_faces(request):
+	with MappedArray(request, "main") as m:
+		# loop over the facial embeddings
+		for encoding in encodings:
+			# attempt to match each face in the input image to our known
+			# encodings
+			matches = face_recognition.compare_faces(data["encodings"],
+				encoding)
+			name = "Unknown" #if face is not recognized, then print Unknown
 
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
+			# check to see if we have found a match
+			if True in matches:
+				# find the indexes of all matched faces then initialize a
+				# dictionary to count the total number of times each face
+				# was matched
+				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+				counts = {}
 
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
+				# loop over the matched indexes and maintain a count for
+				# each recognized face face
+				for i in matchedIdxs:
+					name = data["names"][i]
+					counts[name] = counts.get(name, 0) + 1
 
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
+				# determine the recognized face with the largest number
+				# of votes (note: in the event of an unlikely tie Python
+				# will select first entry in the dictionary)
+				name = max(counts, key=counts.get)
 
-			#If someone in your dataset is identified, print their name on the screen
-			if currentname != name:
-				currentname = name
-				print(currentname)
+				#If someone in your dataset is identified, print their name on the screen
+				if currentname != name:
+					currentname = name
+					print(currentname)
 
-		# update the list of names
-		names.append(name)
+			# update the list of names
+			names.append(name)
 
-	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		# (x, y, w, h) = [c * n // d for c, n, d in zip(f, (w0, h0) * 2, (w1, h1) * 2)]            
-		# cv2.rectangle(m.array, (x, y), (x + w, y + h), (0, 255, 0, 0))
-		
-		# draw the predicted face name on the image - color is in BGR
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 225), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			.8, (0, 255, 255), 2)
+		# loop over the recognized faces
+		for ((top, right, bottom, left), name) in zip(boxes, names):
+			# (x, y, w, h) = [c * n // d for c, n, d in zip(f, (w0, h0) * 2, (w1, h1) * 2)]            
+			# cv2.rectangle(m.array, (x, y), (x + w, y + h), (0, 255, 0, 0))
+			
+			# draw the predicted face name on the image - color is in BGR
+			cv2.rectangle(m.array, (left, top), (right, bottom),
+				(0, 255, 225), 2)
+			y = top - 15 if top - 15 > 15 else top + 15
+			cv2.putText(m.array, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+				.8, (0, 255, 255), 2)
 	
+endcodings = []
 
 picam2.post_callback = draw_faces
 picam2.start(show_preview = True)
@@ -99,12 +101,9 @@ while True:
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
 
-	
-
 	# display the image to our screen
-	cv2.imshow("Facial Detection", frame)
+	#cv2.imshow("Facial Detection", frame)
 	key = cv2.waitKey(1) & 0xFF
-
 	# quit when 'q' key is pressed
 	if key == ord("q"):
 		break
