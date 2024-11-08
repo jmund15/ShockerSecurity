8cd5014b83050f754b56d658d942f1556ec0c7df
Jacob S. Mund <Jacob.Mund@idp.wichita.edu>
2024-11-07 19:08:37 -0600

    ready for stream testing

--- backend/flaskStream.py
+++ backend/flaskStream.py
@@ -1,34 +1,25 @@
 #! /usr/bin/python
 
 # import the necessary packages
-#from picamera2 import Picamera2, MappedArray, Preview
+from picamera2 import Picamera2, MappedArray, Preview
 import numpy as np
 from imutils import paths
 import os
 import threading
-#import face_recognition
-#import imutils
-import pickle
+import face_recognition
+import imutils
 import time 
-#import keyboard
-from flask import Flask, Blueprint
-from flask import render_template, url_for, flash, request, redirect, Response
-from flask_login import LoginManager, login_required, current_user, logout_user
+from flask import Blueprint, Response, render_template, stream_with_context
+from flask_login import login_required
 
-import sqlite3
 import time
 import numpy as np
 import cv2
 
 from flaskModels import Face, CSRFForm
-from flaskLogin import login_manager
-from SQLiteConnect import matchEncodings, addFace, getAllFaces
+from SQLiteConnect import addFace, getAllFaces
 
 stream = Blueprint('stream', __name__, template_folder='../frontend')
-#login_manager = LoginManager()
-#login_manager.init_app(stream)
-# app = Flask(__name__)
-# app.debug=True
 
 face_dir = 'frontend/static'
 dictEncodingStr = "encodings"
@@ -42,28 +33,28 @@ encodings = []
 names = []
 unknown_num = len(list(paths.list_images(face_dir))) + 1 #TODO: FIX!
 print("unknown num: {}".format(unknown_num))
-#Initialize 'currentname' to trigger only when a new person is identified.
-currentname = "Unknown"
+rgb = None
+face_annotations = None
 
-# def init_video():
-#     data = load_known_face_encodings()
+def init_video():
+    load_face_encodings()
 
-#     boxes = []
-#     encodings = []
-#     names = []
-#     unknown_num = len(list(paths.list_images(unknown_dir))) + 1
-#     print("unknown num: {}".format(unknown_num))
-#     #Initialize 'currentname' to trigger only when a new person is identified.
-#     currentname = "Unknown"
+    boxes = []
+    encodings = []
+    names = []
+    unknown_num = len(list(paths.list_images(unknown_dir))) + 1
+    print("unknown num: {}".format(unknown_num))
+    #Initialize 'currentname' to trigger only when a new person is identified.
+    currentname = "Unknown"
 
-#     picam2 = Picamera2()
-#     picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480),}))#, lores={"size": (320, 240), "format": "YUV420"}))
-#     picam2.start_preview(Preview.QTGL, width=1280, height=960)
-#     dest_dir = "MemberVideo"
-#     #picam2.start_and_record_video("memberVideo.mp4", duration=5)
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480),}))#, lores={"size": (320, 240), "format": "YUV420"}))
+    picam2.start_preview(Preview.QTGL, width=1280, height=960)
+    dest_dir = "MemberVideo"
+    #picam2.start_and_record_video("memberVideo.mp4", duration=5)
 
-#     (w0, h0) = picam2.stream_configuration("main")["size"] 
-#     #(w1, h1) = picam2.stream_configuration("lores")["size"]
+    (w0, h0) = picam2.stream_configuration("main")["size"] 
+    #(w1, h1) = picam2.stream_configuration("lores")["size"]
 
 
 @stream.route('/stream', methods=['GET'])
@@ -74,10 +65,10 @@ def show():
     return render_template('stream.html', title='ShockerSecurity', form=form)
 
 
-# @stream.route('/stream/security_footage')
-# @login_required  # Protect the video feed route
-# def stream_footage():
-# 	return Response(get_footage(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@stream.route('/stream/security_footage')
+@login_required  # Protect the video feed route
+def stream_footage():
+    return Response(stream_with_context(get_footage()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def load_face_encodings():
     global encoding_dict
@@ -86,29 +77,15 @@ def load_face_encodings():
     for face in faces:
         encoding_dict[dictEncodingStr].append(face.encodings)
         encoding_dict[dictNamesStr].append(face.name)
-    #return encodingDict
-
-    # for face in faces:
-    #     #TODO: Loop through encodings?
-    #     if face.encodings in faces:
-    #         pass
-    #         #data[face.encodings].extend(face.name)
-    #     else:
-    #         faces[face.encodings] = face.name
-    # # load the known faces and embeddings
-    # print("[INFO] loading encodings + face detector...")
 
 
 def draw_faces(request):
-    global faces
-    global names
-    global encodings
-    global boxes
-    global unknown_num
-    global encoding_dict
-    global unknown_dict
+    global unknown_num, unknown_dict, names, face_annotations
     
     with MappedArray(request, "main") as m:
+        # Clear the annotated_frame and start fresh
+        face_annotations = np.zeros_like(m.array)  # Black frame with same size as original
+        
         # loop over the facial embeddings
         for encoding in encodings:
             matches = face_recognition.compare_faces(encoding_dict[dictEncodingStr], encoding)
@@ -133,13 +110,6 @@ def draw_faces(request):
                 # of votes (note: in the event of an unlikely tie Python
                 # will select first entry in the dictionary)
                 name = max(counts, key=counts.get)
-
-                #If someone in your dataset is identified, print their name on the screen
-                #if currentname != name:
-                #	currentname = name
-                #	print(currentname)
-                #else:
-                #	currentname = "Unknown"
             # update the list of names
             names.append(name)
 
@@ -149,23 +119,21 @@ def draw_faces(request):
             # cv2.rectangle(m.array, (x, y), (x + w, y + h), (0, 255, 0, 0))
             
             # draw the predicted face name on the image - color is in BGR
-            cv2.rectangle(m.array, (left, top), (right, bottom),
-                (0, 255, 225), 0)
+            cv2.rectangle(face_annotations, (left, top), (right, bottom), (0, 255, 225), 0)
             y = top - 15 if top - 15 > 15 else top + 15
-            cv2.putText(m.array, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                .8, (0, 255, 255), 0)
+            cv2.putText(face_annotations, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 0)
 				
 def check_unknown_alert(unknown_encoding):
     global encodings
-    has_unknown = false
+    has_unknown = False
     for encoding in encodings:
         matches = face_recognition.compare_faces(unknown_encoding, encoding)
-        if matches is not None:
+        if any(matches):  # checks if there are matches
             has_unknown = True
             break
     if has_unknown:
         cv2.imwrite("{0}/unknown_{1}.jpg".format(face_dir, unknown_num), rgb)
-        addFace('unknown', False, '', unknown_encoding)
+        addFace('Unknown', False, '', unknown_encoding)
         print("unknown face detected! Send picture to user!")
         print("unknown num: {}".format(unknown_num))
         unknown_num += 1
@@ -175,48 +143,36 @@ def check_unknown_alert(unknown_encoding):
         
         
             
-# def get_footage():
-#     global boxes
-#     global names
-#     global encodings
-#     global unknown_num
+def get_footage():
+    global boxes
+    global names
+    global encodings
+    global unknown_num
+    global rgb
     
-#     picam2.post_callback = draw_faces
-#     picam2.start(show_preview = True)
-#     # loop over frames from the video file stream
-#     while True:
-#         # grab the frame from the threaded video stream and resize it
-#         # to 500px (to speedup processing)
-#         frame = picam2.capture_array()
-#         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         # Detect the fce boxes
-#         boxes = face_recognition.face_locations(rgb)
-#         # compute the facial embeddings for each face bounding box
-#         encodings = face_recognition.face_encodings(rgb, boxes)
-#         names = []
+    picam2.post_callback = draw_faces
+    picam2.start(show_preview = True)
+    # loop over frames from the video file stream
+    while True:
+        # grab the frame from the threaded video stream and resize it
+        # to 500px (to speedup processing)
+        frame = picam2.capture_array()
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        annotated_frame = rgb
+        if face_annotations is not None:
+            annotated_frame = cv2.addWeighted(annotated_frame, 1, face_annotations, 1, 0)
+        # Detect the face boxes
+        boxes = face_recognition.face_locations(rgb)
+        # compute the facial embeddings for each face bounding box
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
 
-#         # Encode the frame in JPEG format
-#         _, buffer = cv2.imencode('.jpg', rgb)
-#         web_frame = buffer.tobytes()
+        # Encode the frame in JPEG format
+        _, buffer = cv2.imencode('.jpg', annotated_frame)
+        web_frame = buffer.tobytes()
         
-#         yield (b'--web_frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#         #if keyboard.is_pressed('q'):
-#         #	break
-#         # display the image to our screen
-#         #cv2.imshow("Facial Detection", frame)
-#         #key = cv2.waitKey(1)# & 0xFF
-#         #if key%256 == 27:
-#         #	break
-#         # quit when 'q' key is pressed
-#         #if key == ord("q"):
-#         #	break
-
-
-#     # do a bit of cleanup
-#     cv2.destroyAllWindows()
-#     picam2.stop_preview()
-#     picam2.stop()
+        yield (b'--web_frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + web_frame + b'\r\n')
 
     
 
